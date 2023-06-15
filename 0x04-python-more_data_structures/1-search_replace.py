b4e78ef82a76ef51248cebec09174c38829dccc4
sayv <savegawugah01@gmail.com>
2023-06-15 05:08:04 +0000

    Fixed code 1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -3,5 +3,5 @@ def search_replace(my_list, search, replace):
     new_list = my_list[:]
     for i in range(len(my_list)):
         if my_list[i] == search:
-            new_list[i] == replace
+            new_list[i] = replace
     return new_list

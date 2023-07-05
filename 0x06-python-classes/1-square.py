73111b2dddf8629f895eb558b2d077fc258609ee
sayv <savegawugah01@gmail.com>
2023-07-05 17:16:59 +0000

    Create private nstance size for square

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""
+Define a class Square
+"""
+
+
+class Square:
+    """Defines a square"""
+
+    def __init__(self, size):
+        """Create a private class instance size
+        Args:
+            size: the size of the square
+        """
+        self.__size = size

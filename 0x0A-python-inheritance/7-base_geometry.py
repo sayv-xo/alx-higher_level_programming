8ed8a7013a25606b22be54ca281369ca069cbcca
sayv <savegawugah01@gmail.com>
2023-07-11 05:30:01 +0000

    Fixed error message

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -22,4 +22,4 @@ class BaseGeometry:
         if type(value) is not int:
             raise TypeError(f'{name} must be an integer')
         if value < 1:
-            raise ValueError(f'{name} must be greater than zero')
+            raise ValueError(f'{name} must be greater than 0')
--- 0x0A-python-inheritance/8-rectangle.py
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -17,6 +17,6 @@ class Rectangle(BaseGeometry):
             height(int): The height
         """
         self.integer_validator('width', width)
-        self.__width = width
         self.integer_validator('height', height)
+        self.__width = width
         self.__height = height

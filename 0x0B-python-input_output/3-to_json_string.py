0ce7153fc7573aff5e3e578d4c76b7b7109c1fe6
sayv <98850746+sayv-xo@users.noreply.github.com>
2023-08-03 21:21:16 +0000

    Delete 0x0B-python-input_output directory

--- 0x0B-python-input_output/3-to_json_string.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-"""JSON to string"""
-import json
-
-
-def to_json_string(my_obj):
-    """Returns he JSON representation of my_obj"""
-    return json.dumps(my_obj)
--- 0x0B-python-input_output/8-class_to_json.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python3
-"""Return dict repr"""
-
-
-def class_to_json(obj):
-    """Return dict representation of simple data structure"""
-    return vars(obj)

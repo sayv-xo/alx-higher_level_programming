c915ff4eb791b08a608185fe036f8be27afd236c
sayv <savegawugah01@gmail.com>
2023-09-12 21:49:05 +0000

    Function that writes string to a text file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Function that writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """Writes a string to a text file
+    Args:
+        filename: Name of file
+        text: String to write
+    Return:
+        Number of characters written
+    """
+    with open(filename, 'w', encoding="utf-8") as f:
+        f.write(text)

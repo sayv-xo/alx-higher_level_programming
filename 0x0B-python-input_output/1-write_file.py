6af0a1c82099767f0f21cf86594d93ba12f25546
sayv <savegawugah01@gmail.com>
2023-09-12 21:42:31 +0000

    Function that adds string to a text file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -11,4 +11,4 @@ def write_file(filename="", text=""):
         Number of characters written
     """
     with open(filename, 'w', encoding="utf-8") as f:
-        f.write(text)
+        return f.write(text)

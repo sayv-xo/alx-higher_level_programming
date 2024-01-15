34e0a58cd131918fa4f15e33cdd0d860d2b41bb3
sayv <98850746+sayv-xo@users.noreply.github.com>
2024-01-15 05:50:46 +0000

    get state id

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+""" script to list all states objects """
+import sys
+from sqlalchemy import (create_engine)
+from model_state import Base, State
+from sqlalchemy.orm.session import sessionmaker, Session
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]))
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    index = 0
+    for state in session.query(State).filter(State.name == (sys.argv[4],)):
+        if state:
+            print('{}'.format(state.id))
+            index = 1
+    if not index:
+        print("Not Found")

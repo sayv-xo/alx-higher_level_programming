1336bb220213c6c10e378e0c59c6c8b45a8f7d7c
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2025-05-25 08:43:35 +0000

    edits to add executable file

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,26 +3,26 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   let max;
-  let max2;
+  let secondMax;
 
   if (Number(process.argv[2]) < Number(process.argv[3])) {
-    max2 = Number(process.argv[2]);
+    secondMax = Number(process.argv[2]);
     max = Number(process.argv[3]);
   } else {
     max = Number(process.argv[2]);
-    max2 = Number(process.argv[3]);
+    secondMax = Number(process.argv[3]);
   }
 
   const n = process.argv.length;
 
   for (let i = 4; i < n; i++) {
     const num = Number(process.argv[i]);
-    if (num > max2 && num < max) {
-      max2 = num;
+    if (num > secondMax && num < max) {
+      secondMax = num;
     } else if (num > max) {
-      max2 = max;
+      secondMax = max;
       max = num;
     }
   }
-  console.log(max2);
+  console.log(secondMax);
 }

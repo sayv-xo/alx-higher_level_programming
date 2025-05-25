2df7e897a7cc06f28967b8a33af0b32d14cdd0bb
sayv-xo <98850746+sayv-xo@users.noreply.github.com>
2025-05-25 08:34:19 +0000

    fixes semistandard lint errors

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,26 +3,26 @@ if (process.argv.length === 2 || process.argv.length === 3) {
   console.log(0);
 } else {
   let max;
-  let max_2;
+  let max2;
 
   if (Number(process.argv[2]) < Number(process.argv[3])) {
-    max_2 = Number(process.argv[2]);
+    max2 = Number(process.argv[2]);
     max = Number(process.argv[3]);
   } else {
     max = Number(process.argv[2]);
-    max_2 = Number(process.argv[3]);
+    max2 = Number(process.argv[3]);
   }
 
-  const n = process.argv.length
+  const n = process.argv.length;
 
-  for (i = 4; i < n; i++) {
-    let num = Number(process.argv[i]);
-    if (num > max_2 && num < max) {
-      max_2 = num;
+  for (let i = 4; i < n; i++) {
+    const num = Number(process.argv[i]);
+    if (num > max2 && num < max) {
+      max2 = num;
     } else if (num > max) {
-      max_2 = max;
+      max2 = max;
       max = num;
     }
   }
-  console.log(max_2)
+  console.log(max2);
 }
